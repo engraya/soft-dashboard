4d55de4f3123c705a8ba53a1e91728d2adc08c5e
Engraya <ahmadyakubuaya@gmail.com>
2024-11-21 00:00:38 +0100

    Added Google Anlytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,23 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-SCQP74FRK3");
+
+// Send a custom event
+ReactGA.event({
+  category: "your category",
+  action: "your action",
+  label: "your label", // optional
+  value: 99, // optional, must be a number
+  nonInteraction: true, // optional, true/false
+  transport: "xhr", // optional, beacon/xhr/image
+});
+
+// Send pageview with a custom path
+ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
